36a27aa1a7705da16f392a1c382a11d1047b2334
KelliePetersen <KellieAnnPetersen@gmail.com>
2018-11-12 18:34:40 -0500

    add fixed menu

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,19 @@
-const Slideshow = require('./modules/Slideshow');
+const FixedMenu = require('./modules/FixedMenu'),
+      Slideshow = require('./modules/Slideshow');
+
+const fixedMenu = new FixedMenu();
+fixedMenu.events();
 
 const slideshow = new Slideshow();
 slideshow.autoSlide();
+
+
+
+
+const img = document.getElementsByClassName("slideshow__image");
+
+document.addEventListener("scroll", function() {
+  for (let i = 0; i < img.length; i++) {
+    img[i].style.top = ("0" + (window.scrollY / 1.8) + "px");
+  }
+});
---
+++ app/assets/scripts/modules/FixedMenu.js
@@ -0,0 +1,36 @@
+class FixedMenu {
+  constructor() {
+    this.topBar = document.getElementsByClassName("top-bar");
+    this.navBar = document.getElementsByClassName("header");
+    this.navBarLogo = document.getElementsByClassName("header__logo");
+  }
+
+  events() {
+    window.addEventListener("scroll", this.scrollMethods.bind(this));
+  }
+
+  scrollMethods() {
+    this.menuOnScroll();
+    this.logoScroll();
+  }
+
+  menuOnScroll() {
+    if (window.scrollY > 32) {
+      this.topBar[0].classList.add("top-bar--fixed-menu");
+      this.navBar[0].classList.add("header--fixed-menu");
+    } else {
+      this.topBar[0].classList.remove("top-bar--fixed-menu");
+      this.navBar[0].classList.remove("header--fixed-menu");
+    }
+  }
+
+  logoScroll() {
+    if (window.scrollY > 70) {
+      this.navBarLogo[0].classList.add("header__logo--fixed-menu");
+    } else {
+      this.navBarLogo[0].classList.remove("header__logo--fixed-menu");
+    }
+  }
+}
+
+module.exports = FixedMenu;
