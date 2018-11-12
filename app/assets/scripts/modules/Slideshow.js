0ade3fa3afda8a5a93ecb2a67e364b11018be06f
KelliePetersen <KellieAnnPetersen@gmail.com>
2018-11-11 23:05:49 -0500

    add rough code for auto-slideshow

--- app/assets/scripts/modules/Slideshow.js
+++ app/assets/scripts/modules/Slideshow.js
@@ -1,3 +1,13 @@
+class Slideshow {
+  constructor() {
+    this.images = document.getElementsByClassName("slideshow__image");
+    this.hidden = "slideshow__image";
+    this.visible = "slideshow__image-visible";
+  }
 
+  autoSlide() {
+    this.images[0].className = this.visible;
+  }
+}
 
 module.exports = Slideshow;
